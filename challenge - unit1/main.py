ff84b81a9626387998c45cba205d8343ff5c832c
loki135 <93985411+loki135@users.noreply.github.com>
2023-09-09 16:37:58 +0530

    Update main.py

--- challenge - unit1/main.py
+++ challenge - unit1/main.py
@@ -1,4 +1,10 @@
-a=10
-b=20
-a,b=b,a
-print(a)
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+number = int(input("Enter a number: "))
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
